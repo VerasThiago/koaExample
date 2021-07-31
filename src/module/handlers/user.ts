177330dbe5a0cc8b42a9cd490ca6e15e3296a003
Thiago Veras <thiago.veras.machado@gmail.com>
2021-07-31 12:02:02 -0300

    Chore/fix post and add docker (#9)
    * Add post request parsing
    
    * Add dockerfile

--- src/module/handlers/user.ts
+++ src/module/handlers/user.ts
@@ -1,5 +1,14 @@
 import Koa from 'koa'
 
+import coBody from 'co-body'
+
+interface Data {
+  login: string
+  password: string
+}
+
 export async function userHandler(ctx: Koa.ParameterizedContext) {
-  ctx.body = 'User Route'
+  const body: Data = await coBody(ctx.req)
+
+  ctx.body = `Welcome ${body.login}!`
 }
